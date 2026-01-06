90459245995d7a97d07207f06bd05e6e1a3cd3bc
Pattikonda Surendra <pattikondasurendra05@gmail.com>
2026-01-07 03:42:30 +0530

    allowed CORS in server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ connectDB();
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:8080",
+  origin: ["http://localhost:8080",
+           "https://club-hub-ds.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
